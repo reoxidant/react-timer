c89badeb42201322185280892151a11c8ff32f0f
Vitaliy Shapovalov <xflash55@yandex.ru>
2020-06-29 12:21:23 +0300

    [ADD] Set value in number input

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -5,12 +5,28 @@ import {TimeControl} from "../TimeControl/TimeControl";
 import {TimeView} from "../TimeView/TimeView";
 
 export class Content extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            timevalue: 0
+        }
+    }
+
+    onSetNewValue(value){
+        this.setState({timevalue: value})
+    }
+
     render() {
+
+        const timevalue = this.state.timevalue;
+
         return (
             <div className="content-circle">
                 <div className="circle">
                     <TimeView/>
-                    <TimeControl/>
+                    <TimeControl timevalue={timevalue} onSetNewValue={this.onSetNewValue.bind(this)}/>
                     <Actions/>
                 </div>
             </div>
--- src/Components/TimeControl/TimeControl.js
+++ src/Components/TimeControl/TimeControl.js
@@ -2,10 +2,19 @@ import React from "react";
 import './TimeControl.css';
 
 export class TimeControl extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleChange(e){
+        this.props.onSetNewValue(e.target.value);
+    }
+
     render() {
         return (
             <div className="time-control">
-                <input className="input-control" type="text" value="0"/>
+                <input className="input-control" type="number" value={this.props.timevalue} onChange={this.handleChange.bind(this)}/>
             </div>
         );
     }
